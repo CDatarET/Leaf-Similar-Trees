d596a1065f93816d869c75b7c1e8421381639f8e
Chaitanya Datar <datar.chaitanya@gmail.com>
2025-09-07 17:13:03 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -33,16 +33,7 @@ class Solution {
         List<Integer> list2 = new ArrayList<>();
         leaves(root1, list1);
         leaves(root2, list2);
-        
-        if(list1.size() != list2.size()){
-            return(false);
-        }
 
-        for(int i = 0; i < list1.size(); i++){
-            if(!list1.get(i).equals(list2.get(i))){
-                return(false);
-            }
-        }
-        return(true);
+        return(list1.equals(list2));
     }
 }
