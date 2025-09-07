881f613266880ffc6a6107004c26a42dbc270d61
Chaitanya Datar <datar.chaitanya@gmail.com>
2025-09-07 17:02:02 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void leaves(TreeNode root, List<Integer> list){
+        if(root == null){
+            return;
+        }
+
+        if(root.left == null && root.right == null){
+            list.add(root.val);
+            return;
+        }
+
+        leaves(root.left, list);
+        leaves(root.right, list);
+    }
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        if(root1 == null || root2 == null){
+            if(root1 == root2){
+                return(true);
+            }
+            return(false);
+        }
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        leaves(root1, list1);
+        leaves(root2, list2);
+        System.out.println(list1);
+        System.out.println(list1);
+        
+
+        if(list1.size() != list2.size()){
+            return(false);
+        }
+
+        for(int i = 0; i < list1.size(); i++){
+            if(!list1.get(i).equals(list2.get(i))){
+                return(false);
+            }
+        }
+        return(true);
+    }
+}
